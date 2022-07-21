5d5fc6e63084163e70685fee142560ef67a2f048
Anne-Hyeyeon <khy2106@naver.com>
2022-07-21 21:21:20 +0900

    영화 앱 완성 + 코인트래커

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coinValue, setCoinValue] = useState(0);
-  const [select, setSelect] = useState(0);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    })
-  }, [])
-  function onChange(e) {
-    setCoinValue(e.target.value);
-  }
-  function selectOnChange(e) {
-    setSelect(e.target.value);
-  }
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading</strong> :  
-        <>
-        <select onChange={selectOnChange}>
-        {coins.map((coin,index) => 
-              <option value={coin.quotes.USD.price} key={index}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</option>)
-        }
-        </select>
-        {select ? <><input 
-        type="number"
-        onChange={onChange}
-        value={coinValue}
-        placeholder="How much do you have?(USD)"
-      />
-        <input
-        type="number"
-        value={coinValue/select}
-        onChange={onChange}
-        /></> : null}
-       </>               
-       }
-    </div>
-  );
-}
+  return(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/movie/:id" element={<Detail />}></Route>
+            <Route path="/" element={<Home />}></Route>
+        </Routes>
+    </BrowserRouter>
+)}
 
 export default App;
